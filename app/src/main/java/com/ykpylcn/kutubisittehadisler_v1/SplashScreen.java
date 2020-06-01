2c5806cb4bac4f0c8a723826e05b1ad70dc0ea24
Yakup YALCIN <yalcin.web@gmail.com>
2020-06-01 23:56:03 +0200

    Commit 06012020-3

--- app/src/main/java/com/ykpylcn/kutubisittehadisler_v1/SplashScreen.java
+++ app/src/main/java/com/ykpylcn/kutubisittehadisler_v1/SplashScreen.java
@@ -23,30 +23,25 @@ public class SplashScreen extends AppCompatActivity {
     /** Duration of wait **/
 
 
-    /**
-     * Some older devices needs a small delay between UI widget updates
-     * and a change of the status and navigation bar.
-     */
-    private static final int UI_ANIMATION_DELAY = 300;
     private final Handler mHideHandler = new Handler();
-    private View mContentView;
-    private final Runnable mHidePart2Runnable = new Runnable() {
-        @SuppressLint("InlinedApi")
-        @Override
-        public void run() {
-            // Delayed removal of status and navigation bar
-
-            // Note that some of these constants are new as of API 16 (Jelly Bean)
-            // and API 19 (KitKat). It is safe to use them, as they are inlined
-            // at compile-time and do nothing on earlier devices.
-            mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                    | View.SYSTEM_UI_FLAG_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-        }
-    };
+//    private View mContentView;
+//    private final Runnable mHidePart2Runnable = new Runnable() {
+//        @SuppressLint("InlinedApi")
+//        @Override
+//        public void run() {
+//            // Delayed removal of status and navigation bar
+//
+//            // Note that some of these constants are new as of API 16 (Jelly Bean)
+//            // and API 19 (KitKat). It is safe to use them, as they are inlined
+//            // at compile-time and do nothing on earlier devices.
+//            mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+//                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+//                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+//                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+//                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+//        }
+//    };
 
 
 
@@ -68,7 +63,7 @@ public class SplashScreen extends AppCompatActivity {
         }, SPLASH_HIDE_DELAY_MILLIS);
 
 //        mControlsView = findViewById(R.id.fullscreen_content_controls);
-        mContentView = findViewById(R.id.fullscreen_content);
+       // mContentView = findViewById(R.id.fullscreen_content);
 
 
         // Upon interacting with UI controls, delay any scheduled hide()
